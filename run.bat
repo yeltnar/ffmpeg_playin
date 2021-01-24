50ce4cac3ec89bc04c2ecc7ff72b3cbbbc138b4e
yeltnar <github@andbrant.com>
2021-01-24 11:26:20 -0600

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,9 @@
+@REM https://trac.ffmpeg.org/wiki/StreamingGuide
+@REM make sure to have Stereo Mix enabled
+
+
+
+
 @REM ffmpeg -f gdigrab -framerate 30 -i desktop output.mkv
 
 @REM ffmpeg -f dshow -i video="UScreenCapture" -f dshow -i audio="Microphone" output.mkv
@@ -6,13 +12,41 @@
 
 
 
-
-
+@REM helpful stuffs
 @REM try to add `-tune zerolatency `
+@REM try to capture one app ffmpeg -f gdigrab -framerate 30 -i title=Calculator output.mkv
+@REM try to add `-c:v h264_nvenc` for hw encoding 
+@REM scale output `-vf scale=320:240 -f rtsp rtsp://localhost:8554/drew`
+
+
 @REM try to add `-b 2255k`
 @REM try to add `-s 1280x720`
-ffmpeg -re -f gdigrab -framerate 30 -i desktop -f dshow -f dshow -i audio="Stereo Mix (Realtek(R) Audio)" -preset ultrafast -tune zerolatency -f rtsp rtsp://localhost:8554/drew
+@REM try to add `-c:v h264_nvenc` for hw encoding 
+@REM ffmpeg -re ^
+@REM -f gdigrab -framerate 23 -i desktop ^
+@REM -f dshow -f dshow -i audio="Stereo Mix (Realtek(R) Audio)" ^
+@REM -c:v libx264_nvenc ^
+@REM -preset ultrafast -vcodec libx264 -tune zerolatency -b 900k ^
+@REM -vf scale=1280:720 ^
+@REM -f flv rtmp://localhost:1935/live
 
+
+@REM trying to get just video to play 
+ffmpeg ^
+-f gdigrab -framerate 30 -i desktop ^
+-f dshow -f dshow -i audio="Stereo Mix (Realtek(R) Audio)" ^
+-c:v h264_nvenc ^
+-c:a aac -b:a 160k -ar 44100 -strict -2 ^
+-preset ultrafast -vcodec libx264 -b 900k ^
+-vf scale=1920:-1 ^
+-f flv rtmp://localhost:1935/live
+
+
+@REM try to capture one app 
+@REM ffmpeg -re ^
+@REM -f gdigrab -framerate 30 -i title="A New Hope - VLC media player" ^
+@REM -f dshow -f dshow -i audio="Stereo Mix (Realtek(R) Audio)" ^
+@REM output.mkv
 
 
 @REM RTSP working
@@ -26,3 +60,22 @@ ffmpeg -re -f gdigrab -framerate 30 -i desktop -f dshow -f dshow -i audio="Stere
 
 @REM fully working
 @REM ffmpeg -re -f gdigrab -framerate 30 -i desktop -f dshow -f dshow -i audio="Stereo Mix (Realtek(R) Audio)" -f rtsp rtsp://localhost:8554/drew
+
+@REM using gpu
+@REM ffmpeg -re -f gdigrab -framerate 30 -i desktop -f dshow -f dshow -i audio="Stereo Mix (Realtek(R) Audio)" -c:v h264_nvenc -f rtsp rtsp://localhost:8554/drew
+
+@REM RTMP working
+@REM ffmpeg -re -i "..\Videos\A New Hope.mp4" -c:a aac -b:a 160k -ar 44100 -strict -2 -preset ultrafast -vcodec libx264 -b 900k -f flv rtmp://localhost:1935/live
+@REM RTMP working better
+@REM ffmpeg -f gdigrab -framerate 30 -i desktop -f dshow -f dshow -i audio="Stereo Mix (Realtek(R) Audio)" -c:a aac -b:a 160k -ar 44100 -strict -2 -preset ultrafast -vcodec libx264 -b 900k -vf scale=1920:-1 -f flv rtmp://localhost:1935/live
+
+@REM seems pretty okay
+@REM ffmpeg -re ^
+@REM -f gdigrab -framerate 23 -i desktop ^
+@REM -f dshow -f dshow -i audio="Stereo Mix (Realtek(R) Audio)" ^
+@REM -c:v libx264_nvenc ^
+@REM -preset ultrafast -vcodec libx264 -tune zerolatency -b 900k ^
+@REM -vf scale=1280:720 ^
+@REM -f rtsp rtsp://localhost:8554/drew
+
+@REM intra-refresh
